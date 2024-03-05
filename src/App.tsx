5f60f3eaab574df7ecf319584eb8c38b77de2590
kanugurajesh <kanugurajesh3@gmail.com>
2024-03-05 11:29:01 +0530

    Adding animation to cursor

--- src/App.tsx
+++ src/App.tsx
@@ -15,17 +15,32 @@ const App = () => {
 
   const cursorRef = useRef(null);
   const clickRef = useRef(null);
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+  const [cursorPosition, setCursorPosition] = useState({
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2,
+  });
 
   const handleMouseMove = (event) => {
     setCursorPosition({ x: event.clientX, y: event.clientY });
   };
 
+  const handleClick = () => {
+    const cursorElement = clickRef.current;
+
+    if (cursorElement.classList.contains("animated-click")) {
+      cursorElement.classList.remove("animated-click");
+    } else {
+      cursorElement.classList.add("animated-click");
+    }
+  };
+
   useEffect(() => {
     const cursorElement = cursorRef.current;
+
     if (cursorElement) {
       cursorElement.addEventListener("mousemove", handleMouseMove);
-      cursorElement.addEventListener("click", () => console.log("Adding"))
+      cursorElement.addEventListener("click", handleClick);
     }
     return () => {
       if (cursorElement) {
@@ -40,8 +55,13 @@ const App = () => {
       ref={cursorRef}
     >
       <div
-        className="w-6 h-6 border-2 border-black rounded-full absolute"
-        style={{ top: cursorPosition.y - 5 , left: cursorPosition.x - 8, zIndex:-1}}
+        className="w-6 h-6 border-2 border-black rounded-full absolute "
+        style={{
+          top: cursorPosition.y - 5,
+          left: cursorPosition.x - 8,
+          zIndex: -1,
+        }}
+        ref={clickRef}
       ></div>
       <h1 className="text-4xl font-bold">React Media Recorder</h1>
       <div className="flex gap-4">
